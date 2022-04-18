735d7260bf3ab60f87de5383d98dab6e12262b58
Florent Le Gall <florent.legall@datadoghq.com>
2022-04-18 22:27:52 +0200

    Add dummy project

---
+++ index.d.ts
@@ -0,0 +1,3 @@
+export declare const verify: () => {
+    onDifferentLines(...elements: HTMLElement[]): void;
+};
---
+++ index.ts
@@ -0,0 +1,3 @@
+export const verify = () => ({
+	onDifferentLines(...elements: HTMLElement[]) {},
+})
